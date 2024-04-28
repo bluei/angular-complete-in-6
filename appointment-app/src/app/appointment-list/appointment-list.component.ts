fcf9e2f24d0142da0348d34b021aec6000156393
Scott Morey <scottrmorey@gmail.com>
2024-04-28 13:18:44 -0400

    finished day 1 - now to day 2 33

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
   
-  newAppointmentTitle: string = ""
+  newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
   appointments: Appointment[] = [];
-  
-  
-  appointment2: Appointment = {
+
+  // example of setting an appointment before creating an array and 2-way binding
+  appointmentOrig: Appointment = {
     id: 1,
     title: "Take dog out",
     date: new Date('2024-04-20')
+  }
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+    
+    // if there are saved appointments, initialize to that, otherwise empty array
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
   }
 
   addAppointment() {
@@ -34,11 +42,17 @@ export class AppointmentListComponent {
       // clear the fields
       this.newAppointmentDate = new Date();
       this.newAppointmentTitle = "";
+
+      // save in local storage (key, value format)
+      localStorage.setItem("appointments", JSON.stringify(this.appointments));
     }
   }
 
 
   deleteAppointment(index: number){
     this.appointments.splice(index, 1);
+
+    // update local storage with the new array (now minus the deleted item)
+    localStorage.setItem("appointments", JSON.stringify(this.appointments));
   }
 }
